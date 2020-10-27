7c11b9ec685a881ac1eddf2002f72cec93314826
A00079 <bajay0007@gmail.com>
2020-10-28 04:05:42 +0530

    Fixed Home Page UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { createBrowserHistory } from "history";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from '../src/views/Landing.js';
+import Home from '../src/views/Home.js';
 
 var hist = createBrowserHistory();
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Router history={hist}>
         <Route exact path="/" component={Landing} />
+        <Route exact path="/Home" component={Home} />
       </Router>
     </div>
   );
